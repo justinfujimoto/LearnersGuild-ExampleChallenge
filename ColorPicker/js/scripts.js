e7b6dbd065b6717a026a78840e88e2ea1dd2fd2e
justinfujimoto <jfujimoto@sfo.yearup.org>
2016-08-03 12:38:12 -0700

    ColorPicker
    1. HTML Structure
    2. (3) buttons -- yellow/red/blue
    attributes:
    type="button"
    onClick="function();"
    3. id="background" attribute to <body> tag
    4. link js file within <head> elements, metadata
    <script src="js/scripts.js"></script>
    <meta charset="utf-8">
    5. scripts.js -- (3) functions
    document.getElementById("background").style.backgroundColor = "color";

---
+++ ColorPicker/js/scripts.js
@@ -0,0 +1,11 @@
+var yellow = function() {
+  var a = document.getElementById("background").style.backgroundColor = "yellow";
+}
+
+var red = function() {
+	var a = document.getElementById("background").style.backgroundColor = "red";
+}
+
+var blue = function() {
+	var a = document.getElementById("background").style.backgroundColor = "blue";
+}
